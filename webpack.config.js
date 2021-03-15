d2e255483b496627424bf84c98a7c780c26b385b
Rovin Vitalii <vitaliirovin@yandex.ru>
2021-03-15 14:48:01 +0700

    new path

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = "/";
+  const publicPath = "/main.28175d91880a2ded5c9a.css";
 
   const pcss = {
     test: /\.(p|post|)css$/,
